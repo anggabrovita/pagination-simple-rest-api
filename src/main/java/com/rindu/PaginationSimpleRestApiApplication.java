f02707d9f599098dcff598e7ae5dd0867d0b0017
Angga Brovita <anggabrovita@gmail.com>
2021-02-05 12:41:43 +0700

    initial commit

---
+++ src/main/java/com/rindu/PaginationSimpleRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.rindu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationSimpleRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationSimpleRestApiApplication.class, args);
+	}
+
+}
